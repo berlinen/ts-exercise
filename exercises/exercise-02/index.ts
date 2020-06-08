66ed561512328659fc4c48fc95a47619cc20efa9
“berlinen” <1292393108@qq.com>
2020-06-08 11:28:50 +0800

    feat: 联合类型 泛型

---
+++ exercises/exercise-02/index.ts
@@ -0,0 +1,84 @@
+import chalk from 'chalk';
+
+/*
+Intro:
+    Since we already have some of the additional
+    information about our users, it's a good idea
+    to output it in a nice way.
+Exercise:
+    Fix type errors in logPerson function.
+    logPerson function should accept both User and Admin
+    and should output relevant information according to
+    the input: occupation for User and role for Admin.
+Run:
+    npm run 2
+    - OR -
+    yarn -s 2
+*/
+
+interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    name: string;
+    age: number;
+    role: string;
+}
+
+type Person = User | Admin;
+
+const persons: Person[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+
+function logPerson(person: Person) {
+    let additionalInformation: string;
+    if ((<Admin>person).role) {
+        additionalInformation = (<Admin>person).role;
+    } else {
+        additionalInformation = (<User>person).occupation;
+    }
+    console.log(` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`);
+}
+
+// or
+
+function isAdmin(person: Person): person is Admin {
+  return (<Admin>person).role !== undefined
+}
+
+function logPersonOr(person: Person) {
+  let additionalInformation: string;
+  if(isAdmin(person)) {
+    additionalInformation = person.role
+  } else {
+    additionalInformation = person.occupation
+  }
+
+  console.log(` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`);
+}
+
+persons.forEach(logPerson);
+persons.forEach(logPersonOr)
